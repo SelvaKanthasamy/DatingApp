94baede67774d43ec0eedd71f07eed8e25adbaf5
Selvam Kanthasamy <selva.kanthasamy17@gmail.com>
2021-06-01 13:38:46 +0530

    Client Added

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +57,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseEndpoints(endpoints =>
             {
