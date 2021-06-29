4cdde310c39b027433a8376fec237b463516c106
Selvam Kanthasamy <selva.kanthasamy17@gmail.com>
2021-06-29 18:10:51 +0530

    M - M Rlnsp, configure Dbcontext added

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr';
+import { MembersService } from './../../_services/members.service';
 import { Router } from '@angular/router';
 import { Component, Input, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
@@ -14,13 +16,23 @@ export class MemberCardComponent implements OnInit {
 
   @Input() member: Member;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private memberService: MembersService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
-  btnClick= function (username) {
+  // btnClick= function (username) {
+  //   this.router.navigateByUrl('/members/' + username);
+  // };
+
+  viewProfile(username: string){
     this.router.navigateByUrl('/members/' + username);
-};
+  }
+
+  addLike(member: Member){
+    this.memberService.addLike(member.username).subscribe(() => {
+      this.toastr.success('You have liked ' + member.knownAs);
+    })
+  }
 
 }
